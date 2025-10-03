68ab5e8f8cdcb9ca44fc351a5594a54a69e6a34d
Yevhenii Orhanistyi <organistiy@gmail.com>
2025-10-03 08:53:18 +0200

    feat: add app providers

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
-import { Nunito } from "next/font/google";
+import { Nunito } from 'next/font/google';
+import { Providers } from '@/app/providers';
 
-import "./globals.css";
+import './globals.css';
 
 const nunito = Nunito({
   subsets: ['cyrillic'],
@@ -8,19 +9,21 @@ const nunito = Nunito({
   weight: ['400', '500', '600', '700', '800', '900'],
 });
 
-export default function RootLayout({
+export default function AppLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${nunito.className} antialiased`}
-      >
-        <main className='min-h-screen'>
-          {children}
-        </main>
+    <html lang="en" data-scroll-behavior="smooth">
+      <head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
+        <link rel="manifest" href="/favicons/site.webmanifest" />
+      </head>
+      <body className={nunito.className}>
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
