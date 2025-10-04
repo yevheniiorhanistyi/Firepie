abc7d05f1141551af9cf4429d8cb09f4eb26d581
Yevhenii Orhanistyi <organistiy@gmail.com>
2025-10-04 11:41:02 +0200

    fix: fix issue with correct phone number length

--- shared/constants/checkout-form-schema.ts
+++ shared/constants/checkout-form-schema.ts
@@ -4,7 +4,7 @@ export const checkoutFormSchema = z.object({
   firstName: z.string().min(2, { message: 'Name should contain at least 2 characters' }),
   lastName: z.string().min(2, { message: 'Last name should contain at least 2 characters' }),
   email: z.email({ message: 'Enter a valid email' }),
-  phone: z.string().min(10, { message: 'Enter a valid phone number' }),
+  phone: z.string().min(9, { message: 'Enter a valid phone number' }),
   address: z.string().min(5, { message: 'Enter a valid address' }),
   notes: z.string().optional(),
 });
