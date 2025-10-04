6f183b3aaec129a83e27e6144659d6bcd9c3b3e6
Yevhenii Orhanistyi <organistiy@gmail.com>
2025-10-04 12:11:36 +0200

    feat: add Vercel Analytics

--- app/providers.tsx
+++ app/providers.tsx
@@ -4,6 +4,7 @@ import React from 'react';
 import NextTopLoader from 'nextjs-toploader';
 import { Toaster } from 'react-hot-toast';
 import { SessionProvider } from "next-auth/react"
+import { Analytics } from "@vercel/analytics/next"
 
 export const Providers: React.FC<React.PropsWithChildren> = ({ children }) => {
   return (
@@ -14,6 +15,7 @@ export const Providers: React.FC<React.PropsWithChildren> = ({ children }) => {
         color="#ff6900"
         showSpinner={false}
       />
+      <Analytics />
     </>
   );
 };
